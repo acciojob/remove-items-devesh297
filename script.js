b17dc1e2a5d42d2593d33c2a38c7fa976ee3a94d
Devesh Goswami <53081430+devesh297@users.noreply.github.com>
2025-03-25 00:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  const select = document.getElementById("colorSelect");
+  const button = document.querySelector("input[type='button']");
+
+  button.addEventListener("click", () => {
+    const selectedOption = select.options[select.selectedIndex];
+    if (selectedOption) {
+      selectedOption.remove(); 
+    }
+  });
+});
